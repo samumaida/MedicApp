a7688df87fd2c3a20e42ce3408b4b2e9b834245d
Samuele Maida <samumaida@gmail.com>
2026-07-07 13:14:11 +0200

    Miglioria acquisizione chiave
    - Ora la chiave JWT viene recuperata correttamente dall'API di ConfigService e non è più hardcodata nel codice

--- med-booking-backend/src/auth/admin.guard.ts
+++ med-booking-backend/src/auth/admin.guard.ts
@@ -1,9 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, ForbiddenException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
@@ -17,7 +21,7 @@ export class AdminGuard implements CanActivate {
 
     try {
       const payload = this.jwtService.verify(token, {
-        secret: 'CHIAVE_SEGRETA_SUPER_DIFFICILE_PER_LA_TESI_2026',
+        secret: this.configService.get<string>('JWT_SECRET'),
       });
 
       if (payload.ruolo !== 'admin') {
